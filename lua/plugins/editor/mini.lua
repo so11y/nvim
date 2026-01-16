c5d8df3ace0c9698c58a8b6901d82e0052d37b1c
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-16 17:23:55 +0800

    feat: version

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -1,7 +1,13 @@
-return {
+return {{
     "echasnovski/mini.ai",
     event = "VeryLazy",
     config = function(_, opts)
         require("mini.ai").setup(opts)
     end
-}
+}, {
+    "echasnovski/mini.pairs",
+    event = "InsertEnter",
+    config = function(_, opts)
+        require('mini.pairs').setup(opts)
+    end
+}}
