5785a75d5c76eff6a445fff3235cf714a6bebd6f
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-16 10:49:00 +0800

    feat: 1

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,37 +1,37 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
-    config = function()
-        local ok, ts_config = pcall(require, "nvim-treesitter.config")
-        if ok then
-            package.preload["nvim-treesitter.configs"] = function()
-                return ts_config
-            end
-        end
+    -- dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
+    -- config = function()
+    --     local ok, ts_config = pcall(require, "nvim-treesitter.config")
+    --     if ok then
+    --         package.preload["nvim-treesitter.configs"] = function()
+    --             return ts_config
+    --         end
+    --     end
 
-        -- 3. 使用新版正确的单数 'config'
-        local configs = require("nvim-treesitter.config")
+    --     -- 3. 使用新版正确的单数 'config'
+    --     local configs = require("nvim-treesitter.config")
 
-        configs.setup({
-            ensure_installed = {"lua", "vim", "vimdoc", "javascript", "typescript", "vue", "html", "css", "query"},
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer"
-                    }
-                }
-            }
-        })
-    end
+    --     configs.setup({
+    --         ensure_installed = {"lua", "vim", "vimdoc", "javascript", "typescript", "vue", "html", "css", "query"},
+    --         highlight = {
+    --             enable = true
+    --         },
+    --         indent = {
+    --             enable = true
+    --         },
+    --         textobjects = {
+    --             select = {
+    --                 enable = true,
+    --                 lookahead = true,
+    --                 keymaps = {
+    --                     ["af"] = "@function.outer",
+    --                     ["if"] = "@function.inner",
+    --                     ["ac"] = "@class.outer"
+    --                 }
+    --             }
+    --         }
+    --     })
+    -- end
 }
