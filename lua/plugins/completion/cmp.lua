c202dbed86f0cde8367532f9e8cbd28763ad2f7d
so11y <247076126@qq.com>
2026-01-18 16:50:31 +0800

    featr: version

--- lua/plugins/completion/cmp.lua
+++ lua/plugins/completion/cmp.lua
@@ -77,12 +77,12 @@ return {
                 }),
 
                 ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
+                    if luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif cmp.visible() then
                         cmp.confirm({
                             select = true
                         })
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
                     else
                         fallback()
                     end
