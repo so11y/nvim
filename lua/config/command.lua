b09b70efa99d6e73316a8826d20cfdc899c7c0eb
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-28 12:12:00 +0800

    feat: version

---
+++ lua/config/command.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_user_command('ToggleZen', function()
+    if vim.opt.number:get() then
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+        vim.opt.signcolumn = "no"
+    else
+        vim.opt.number = true
+        vim.opt.relativenumber = true
+        vim.opt.signcolumn = "yes"
+    end
+end, {})
