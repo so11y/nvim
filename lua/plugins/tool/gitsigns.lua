1607f1ea744103c91a0147ff7c023e88e971f7d2
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-28 13:46:11 +0800

    feat: version

--- lua/plugins/tool/gitsigns.lua
+++ lua/plugins/tool/gitsigns.lua
@@ -2,30 +2,10 @@ return {
     "lewis6991/gitsigns.nvim",
     event = {"BufReadPre", "BufNewFile"},
     opts = {
-        signs = {
-            add = {
-                text = '┃'
-            },
-            change = {
-                text = '┃'
-            },
-            delete = {
-                text = '_'
-            },
-            topdelete = {
-                text = '‾'
-            },
-            changedelete = {
-                text = '~'
-            },
-            untracked = {
-                text = '┆'
-            }
-        },
-        -- 开启实时行内 Blame (可选)
+        signcolumn = false,
         current_line_blame = true,
         current_line_blame_opts = {
-            delay = 500 -- 停顿半秒后显示
+            delay = 300
         }
     }
 }
