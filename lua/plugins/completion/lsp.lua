3b4c256310eacf7203f1c425285b63257fd23986
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-29 16:22:10 +0800

    feat: version

--- lua/plugins/completion/lsp.lua
+++ lua/plugins/completion/lsp.lua
@@ -50,9 +50,9 @@ return { -- 1. Mason & LSP 相关
                     },
                     signs = {
                         text = {
-                            [vim.diagnostic.severity.ERROR] = '•',--●
-                            [vim.diagnostic.severity.WARN] = '•',
-                            [vim.diagnostic.severity.INFO] = '•',
+                            [vim.diagnostic.severity.ERROR] = '󰧞',--● •
+                            [vim.diagnostic.severity.WARN] = '󰧞',
+                            [vim.diagnostic.severity.INFO] = '󰧞',
                             [vim.diagnostic.severity.HINT] = '󱐋'
                         }
                     },
