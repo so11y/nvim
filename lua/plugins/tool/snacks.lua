555a1c225386842ee441f4d4e975aa08dacbde24
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-01-30 17:22:50 +0800

    feat: version

--- lua/plugins/tool/snacks.lua
+++ lua/plugins/tool/snacks.lua
@@ -14,20 +14,20 @@ return {{
                 keys = {{
                     icon = " ",
                     key = "f",
-                    desc = "Find File",
-                    action = ":lua Snacks.dashboard.pick('files')"
-                }, {
-                    icon = " ",
-                    key = "n",
-                    desc = "New File",
-                    action = ":ene | startinsert"
-                }, {
+                    desc = "Find projects",
+                    action = ":lua  Snacks.picker.projects()"
+                }, --  {
+                --     icon = " ",
+                --     key = "n",
+                --     desc = "New File",
+                --     action = ":ene | startinsert"
+                -- }, 
+                {
                     icon = " ",
                     key = "g",
                     desc = "Find Text",
                     action = ":lua Snacks.dashboard.pick('live_grep')"
-                }, 
-                -- {
+                }, -- {
                 --     icon = " ",
                 --     key = "r",
                 --     desc = "Recent Files",
