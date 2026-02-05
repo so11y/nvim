33a379aade2190980a8c0ed6e20824017af0a8c9
Sorry <5646769+sorry1@user.noreply.gitee.com>
2026-02-05 15:56:05 +0800

    feat: version

--- lua/plugins/completion/cmp.lua
+++ lua/plugins/completion/cmp.lua
@@ -1,92 +1,98 @@
 return {
-    "saghen/blink.cmp",
-    version = '1.*',
-    event = {"InsertEnter", "CmdlineEnter"},
-    dependencies = {"rafamadriz/friendly-snippets"},
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        snippets = {
-            preset = 'default'
-        },
-        completion = {
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 300
-            },
-            list = {
-                selection = {
-                    auto_insert = false
-                }
-            },
-            ghost_text = {
-                enabled = true
-            },
-            menu = {
-                border = 'rounded',
-                draw = {
-                    treesitter = {'lsp'},
-                    columns = {{
-                        "kind_icon",
-                        gap = 1
-                    }, {
-                        "label",
-                        "label_description",
-                        gap = 1
+	"saghen/blink.cmp",
+	version = "1.*",
+	event = { "InsertEnter", "CmdlineEnter" },
+	dependencies = { "rafamadriz/friendly-snippets" },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		snippets = {
+			preset = "default",
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 300,
+			},
+			list = {
+				selection = {
+					auto_insert = false,
+				},
+			},
+			ghost_text = {
+				enabled = true,
+			},
+			menu = {
+				border = "rounded",
+				draw = {
+					treesitter = { "lsp" },
+					columns = {
+						{
+							"kind_icon",
+							gap = 1,
+						},
+						{
+							"label",
+							"label_description",
+							gap = 1,
+						},
+						{ "kind" },
+					},
+				},
+			},
+		},
 
-                    }, {"kind"}}
-                }
-            }
-        },
+		keymap = {
+			preset = "none",
+			-- 基础控制
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
+			-- S-Tab: 片段回退
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
 
-        keymap = {
-            preset = 'none',
-            -- 基础控制
-            ['<C-space>'] = {'show', 'show_documentation', 'hide_documentation'},
-            ['<C-e>'] = {'hide'},
-            ['<CR>'] = {'accept', 'fallback'},
-            ['<Tab>'] = {'select_and_accept', 'snippet_forward', 'fallback'},
-            -- S-Tab: 片段回退
-            ['<S-Tab>'] = {'snippet_backward', 'fallback'},
-            ['<Up>'] = {'select_prev', 'fallback'},
-            ['<Down>'] = {'select_next', 'fallback'},
+			["<A-k>"] = { "select_prev", "fallback" },
+			["<A-j>"] = { "select_next", "fallback" },
 
-            ['<A-k>'] = {'select_prev', 'fallback'},
-            ['<A-j>'] = {'select_next', 'fallback'}
+			-- -- 命令行滚动文档
+			-- ['<C-u>']     = { 'scroll_documentation_up', 'fallback' },
+			-- ['<C-d>']     = { 'scroll_documentation_down', 'fallback' },
+		},
 
-            -- -- 命令行滚动文档
-            -- ['<C-u>']     = { 'scroll_documentation_up', 'fallback' },
-            -- ['<C-d>']     = { 'scroll_documentation_down', 'fallback' },
-        },
+		cmdline = {
+			enabled = true,
+			completion = {
+				trigger = {
+					show_on_trigger_character = false,
+				},
+				menu = {
+					auto_show = true,
+				},
+				list = {
+					selection = {
+						auto_insert = false,
+					},
+				},
+			},
+			sources = function()
+				return { "path", "cmdline" }
+			end,
+			keymap = {
+				["<Tab>"] = { "show", "accept", "fallback" },
+				["<CR>"] = { "accept_and_enter", "fallback" }, -- 选中并执行命令
+				["<Down>"] = { "select_next", "fallback" },
+				["<Up>"] = { "select_prev", "fallback" },
+				["<A-j>"] = { "select_next", "fallback" },
+				["<A-k>"] = { "select_prev", "fallback" },
+			},
+		},
+	},
 
-        cmdline = {
-            enabled = true,
-            completion = {
-                menu = {
-                    auto_show = true
-                },
-                list = {
-                    selection = {
-                        auto_insert = false
-                    }
-                }
-            },
-            sources = function()
-                return {'path', 'cmdline'}
-            end,
-            keymap = {
-                ['<Tab>'] = {'show', 'accept', 'fallback'},
-                ['<CR>'] = {'accept_and_enter', 'fallback'}, -- 选中并执行命令
-                ['<Down>'] = {'select_next', 'fallback'},
-                ['<Up>'] = {'select_prev', 'fallback'},
-                ['<A-j>'] = {'select_next', 'fallback'},
-                ['<A-k>'] = {'select_prev', 'fallback'}
-            }
-        }
-    },
-
-    config = function(_, opts)
-        local blink = require("blink.cmp")
-        blink.setup(opts)
-    end
+	config = function(_, opts)
+		local blink = require("blink.cmp")
+		blink.setup(opts)
+	end,
 }
